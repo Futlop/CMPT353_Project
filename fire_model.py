66aba428c0160f7b95a300aa49e663fbf956108a
Nick Apostolopoulos <nra45@sfu.ca>
2024-04-11 14:10:37 -0700

    Updated machine learning script to predict size classes of fires.  Also changed some models since they don't work with the data sets.  Machine learning script now produces a csv of training and validation scores

--- fire_model.py
+++ fire_model.py
@@ -1,59 +1,43 @@
-#%%
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import FunctionTransformer
 from scipy import stats
 from sklearn.pipeline import make_pipeline
+from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LinearRegression
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.neural_network import MLPRegressor
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 import matplotlib.pyplot as plt
-
-
 import sys
-# Update imports as needed
 
-def plot_errors(model, X_valid, y_valid):
-    residuals = y_valid - model.predict(X_valid)
-    plt.subplot(1, 2, 1)
-    plt.hist(residuals['fire_spread_rate'])
-    plt.subplot(1, 2, 2)
-    plt.hist(residuals['duration'])
-    plt.show()
-    plt.close()
+# UNUSED
+#def plot_errors(model, X_valid, y_valid):
+#    residuals = y_valid - model.predict(X_valid)
+#    plt.subplot(1, 2, 1)
+#    plt.hist(residuals['fire_spread_rate'])
+#    plt.subplot(1, 2, 2)
+#    plt.hist(residuals['duration'])
+#    plt.show()
+#    plt.close()
 
-# pd.set_option('display.max_rows', None)
-
-# Currently set to only take one input file will update as needed
-file1 = 'fire_data.csv'
+file1 = sys.argv[1]
 
 data = pd.read_csv(file1)
 
 # Separate data into X and y training and validation sets
-data['fire_start_date'] = pd.to_datetime(data['fire_start_date'], errors='coerce')
-data['uc_fs_date'] = pd.to_datetime(data['uc_fs_date'], errors='coerce')
-data['ia_arrival_at_fire_date'] = pd.to_datetime(data['ia_arrival_at_fire_date'], errors='coerce')
-mask = (-pd.isna(data['fire_start_date'])) | (-pd.isna(data['uc_fs_date']))
-data = data[pd.notna(data['fire_start_date']) & pd.notna(data['uc_fs_date'])]
-data['duration'] = (data['uc_fs_date'] - data['fire_start_date']).dt.total_seconds()
-data['response_time'] = (data['uc_fs_date'] - data['ia_arrival_at_fire_date']).dt.total_seconds()
-data = data[pd.notna(data['duration']) & pd.notna(data['response_time'])]
-# X = data[['current_size', 'size_class', 'weather_conditions_over_fire', 'temperature', 'relative_humidity', 'wind_speed', 'response_time']]
-X = data[['size_class', 'temperature', 'relative_humidity', 'wind_speed', 'response_time']]
-y = data[['fire_spread_rate', 'duration']]
-# print(y)
+X = data[['fire_year', 'current_size', 'fire_location_latitude', 'fire_location_longitude', 'fire_spread_rate', 'temperature', 'relative_humidity', 'wind_speed']].values
+y = data[['size_class']].values
 X_train, X_valid, y_train, y_valid = train_test_split(X, y)
 
-# %%
-# Linear regression
+# UNUSED
 # convert size_class to numeric value eg. A, B, C, D, ... -> 1, 2, 3, 4, ...
 # print(data['wind_direction'].unique())
 # print(data['size_class'].unique())
 # print(data['weather_conditions_over_fire'].unique())
-def cat2num(df):
+# def cat2num(df):
     # Define a dictionary mapping wind direction labels to numeric values
     # wind_direction_mapping = {
     #     'N': 0,
@@ -72,14 +56,14 @@ def cat2num(df):
     # # Drop the original wind_direction column
     # df.drop(columns=['wind_direction'], inplace=True)
 
-    size_class_mapping = {
-        'A': 0, 
-        'B': 1, 
-        'C': 2, 
-        'D': 3,
-        'E': 4
-    }
-    df['size_class'] = df['size_class'].map(size_class_mapping)
+    # size_class_mapping = {
+        # 'A': 0, 
+        # 'B': 1, 
+        # 'C': 2, 
+        # 'D': 3,
+        # 'E': 4
+    # }
+    # df['size_class'] = df['size_class'].map(size_class_mapping)
     # weather_condition_mapping = {
     #     'Cloudy': 0, 
     #     'Clear': 1, 
@@ -88,52 +72,35 @@ def cat2num(df):
     #     'CB Wet': 4
     # }
     # df['weather_conditions_over_fire'] = df['weather_conditions_over_fire'].map(weather_condition_mapping)
-    return df
+    # return df
 
-linear_model = make_pipeline(
-    FunctionTransformer(cat2num), 
-    LinearRegression(fit_intercept=True)
-)
-linear_X_train = X_train.copy()
-linear_X_valid = X_valid.copy()
-linear_model.fit(linear_X_train, y_train)
-print(linear_model.score(linear_X_valid, y_valid))
-plot_errors(linear_model, X_valid.copy(), y_valid)
+# Naive Bayes
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+gnb_train = gnb.score(X_train, y_train)
+gnb_valid = gnb.score(X_valid, y_valid)
+# plot_errors(gnb, X_valid, y_valid)
 
+# K Nearest Neighbors
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train, y_train)
+knn_train = knn.score(X_train, y_train)
+knn_valid = knn.score(X_valid, y_valid)
+# plot_errors(knn, X_valid, y_valid)
 
-# %%
-# k nearest neighbors
-# print(X_train)
-# print(y_train)
-kNN_model = make_pipeline(
-    FunctionTransformer(cat2num),
-    StandardScaler(), 
-    KNeighborsRegressor(n_neighbors=5)
-)
-kNN_X_train = X_train.copy()
-kNN_X_valid = X_valid.copy()
-kNN_model.fit(kNN_X_train, y_train)
-print(kNN_model.score(kNN_X_valid, y_valid))
-plot_errors(kNN_model, X_valid.copy(), y_valid)
-# %%
-# random forest
-rf_X_train = X_train.copy()
-rf_X_valid = X_valid.copy()
-rf_model = make_pipeline(
-    FunctionTransformer(cat2num), 
-    RandomForestRegressor()    
-)
-# rf_model = RandomForestRegressor()
-rf_model.fit(rf_X_train, y_train)
-print(rf_model.score(rf_X_valid, y_valid))
-# %%
-# neural network regressor
-mlp_X_train = X_train.copy()
-mlp_X_valid = X_valid.copy()
-mlp_model = make_pipeline(
-    FunctionTransformer(cat2num), 
-    MLPRegressor(hidden_layer_sizes=(8, 6),activation='logistic', solver='lbfgs')
-)
-mlp_model.fit(mlp_X_train, y_train)
-print(mlp_model.score(mlp_X_valid, y_valid))
-# %%
+# Random Forest Classifier
+rfc = RandomForestClassifier(max_depth=4)
+rfc.fit(X_train, y_train)
+rfc_train = rfc.score(X_train, y_train)
+rfc_valid = rfc.score(X_valid, y_valid)
+
+# Neural Network Classifier
+mlp = MLPClassifier(hidden_layer_sizes=(8, 6),activation='logistic', solver='lbfgs')
+mlp.fit(X_train, y_train)
+mlp_train = mlp.score(X_train, y_train)
+mlp_valid = mlp.score(X_valid, y_valid)
+
+# Create dataframe with ML scores
+scores = pd.DataFrame({'Score': ['Training', 'Validation'], 'Naive Bayes': [gnb_train, gnb_valid], 'K Nearest Neighbours': [knn_train, knn_valid], 'Random Forest Classifier': [rfc_train, rfc_valid], 'Neural Network Classifier': [mlp_train, mlp_valid]})
+scores = scores.set_index('Score')
+scores.to_csv("ML_scores.csv")
